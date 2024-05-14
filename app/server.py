4532482e994eed026b0d242946dc3016f13ac64f
Aidan Boling <aidanboling@gmail.com>
2024-05-14 02:07:33 -0700

    Added ability to set which table fields displayed

--- app/server.py
+++ app/server.py
@@ -5,11 +5,18 @@ from job_db_control import JobDbControl
 import job_searcher 
 
 from data.table import job_fields
+from user_settings import SearchSettings, DataDisplay, SettingsControl
 
 ROOT_DIR = Path(__file__).parent
 
 app = Flask(__name__)
 app.secret_key = 'a super secret key'
+
+search_settings_controller = SettingsControl(SearchSettings, 'settings')
+search_settings = search_settings_controller.get_as_dataclass()
+
+table_settings_controller = SettingsControl(DataDisplay, 'data_display')
+table_settings = table_settings_controller.get_as_dataclass()
 
 # # Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:////{ROOT_DIR / "instance/jobs.db"}'
@@ -21,17 +28,28 @@ with app.app_context():
 
 
 def main():
-    @app.route("/")
+    @app.route('/')
     def home():
-        return render_template('index.html', jobs=db_control.get_all(), display_fields=display_fields())
+        #later -> if table_settings.view == 'table', redirect to table-view. else, redirect to list view.
+        return render_template('index.html', jobs=db_control.get_all(), display_fields=display_fields(), options=table_settings)
     
 
-    @app.route("/settings")
+    @app.post('/table-view/update/settings/<setting>')
+    def change_table_settings(setting):
+        settings = list(table_settings.__dict__.keys())
+        if request.form and setting in settings:
+            if setting == 'job_fields':
+                update_fields_displayed(request.form)
+
+        return redirect(url_for('home'))
+
+
+    @app.route('/settings')
     def settings():
         return render_template('settings.html')
 
 
-    @app.get("/settings/run-search")
+    @app.get('/settings/run-search')
     def run_job_search():
         search_and_save_jobs()
         return redirect(url_for('settings'))
@@ -39,7 +57,7 @@ def main():
 
 def search_and_save_jobs():
     
-    db_control = JobDbControl(db)
+    # db_control = JobDbControl(db)
     jobs = job_searcher.run_search(db_control)
     
     if jobs:
@@ -47,11 +65,23 @@ def search_and_save_jobs():
 
 
 def display_fields():
-    return [field for field in job_fields if not field['disabled']]
+    return [field for field in table_settings.job_fields if not field['disabled']]
+
+
+def update_fields_displayed(data: dict):
+    global table_settings
+
+    fields_to_display = data.getlist('display')                    
+    for field in table_settings.job_fields:
+        if field['name'] in fields_to_display:
+            field['disabled'] = False
+        else:
+            field['disabled'] = True
+    
+    table_settings_controller.save_to_file(table_settings)
 
 
 main()
-
 
 
 if __name__ == '__main__':
