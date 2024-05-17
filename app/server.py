3945f1426047737229931596d4397fc56aad79ce
Aidan Boling <aidanboling@gmail.com>
2024-05-17 02:34:48 -0700

    Add view/update for search settings

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ ROOT_DIR = Path(__file__).parent
 app = Flask(__name__)
 app.secret_key = 'a super secret key'
 
-search_settings_controller = SettingsControl(SearchSettings, 'settings')
+search_settings_controller = SettingsControl(SearchSettings, 'search_settings')
 search_settings = search_settings_controller.get_as_dataclass()
 
 table_settings_controller = SettingsControl(DataDisplay, 'data_display')
@@ -40,7 +40,7 @@ def main():
 
 
     @app.post('/table-view/update/settings/<setting>')
-    def change_table_settings(setting):
+    def update_table_settings(setting):
         settings = list(table_settings.__dict__.keys())
         if request.form and setting in settings:
             if setting == 'job_fields':
@@ -49,34 +49,37 @@ def main():
         return redirect(url_for('home'))
 
 
-    @app.route('/settings')
+    @app.get('/settings')
     def settings():
-        return render_template('settings.html')
+        return render_template('settings.html', search_settings=search_settings)
 
 
+    # Later TODO: Add Celery, so can run search in background, and redirect immediately. 
     @app.get('/settings/run-search')
     def run_job_search():
         search_and_save_jobs()
         return redirect(url_for('settings'))
+    
+
+    @app.post('/settings/update/search_settings')
+    def update_search_settings():
+        if request.form:
+            update_search_settings_obj(request.form)
+            search_settings_controller.save_to_file(search_settings)                
+        return redirect(url_for('settings'))
 
 
 def search_and_save_jobs():
-    
-    # db_control = JobDbControl(db)
-    jobs = job_searcher.run_search(db_control)
+    jobs = job_searcher.run_search(db_control, search_settings)
     
     if jobs:
         db_control.add_many(jobs)
 
 
-# def display_fields():
-#     return [field for field in table_settings.job_fields if not field['hidden']]
-
-
 def update_fields_displayed(data: dict):
     global table_settings
 
-    fields_to_display = data.getlist('display')                    
+    fields_to_display = data.getlist('display')                   
     for field in table_settings.job_fields:
         if field['name'] in fields_to_display:
             field['hidden'] = False
@@ -86,6 +89,16 @@ def update_fields_displayed(data: dict):
     table_settings_controller.save_to_file(table_settings)
 
 
+def update_search_settings_obj(data: dict):
+    global search_settings
+
+    # Search phrases
+    search_settings.search_phrases = [phrase.strip() for phrase in data['search_phrases'].split(', ')]
+
+    # Exclude companies list
+    search_settings.exclude_companies = [company.strip() for company in data['exclude_companies'].split(', ')]
+
+    
 main()
 
 
